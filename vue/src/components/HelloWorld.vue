6b4ff497e56027afb19e951d3322272487a1afc4
youyou <1094465220@qq.com>
2019-08-31 18:31:56 +0800

    vue

---
+++ vue/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    {{count}}<button @click="add()">+</button><button @click="down()">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      count : 0
+    }
+  },
+  methods : {
+    add : function() {
+      this.count++;
+    },
+    down : function() {
+      this.count--;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
